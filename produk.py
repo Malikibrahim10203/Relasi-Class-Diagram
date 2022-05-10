e7c50f2408540460520773f2d17218f8cb11dc4a
rayhangdrp <mr2021649@gmail.com>
2022-05-10 15:47:35 +0700

    produk.py

---
+++ produk.py
@@ -0,0 +1,18 @@
+from abc import ABC
+class Produk:
+    
+    def set_nama(self, nama : str):
+        self.__nama=nama
+        
+    def get_nama()-> str:
+        return self.__nama
+    
+    def set_harga(self, harga: float):
+        self.__harga=harga
+        
+    def get_harga()->float:
+        return self.__harga
+    
+    
+    def show_produk(self):
+        pass
