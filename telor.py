b3f632f5e423ebb59d770c60eddf6a7b9e1b54dd
anantapk03 <anantapk03@gmail.com>
2022-05-10 15:59:56 +0700

    telor.py

---
+++ telor.py
@@ -0,0 +1,11 @@
+from produk import Produk
+
+class Telur (Produk) :
+    
+    def show_produk (self) :
+        print ("Nama produk ", self.__nama , "Harga pproduk ", self.__harga)
+        
+        
+    
+    
+    
