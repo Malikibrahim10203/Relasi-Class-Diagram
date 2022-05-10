15df17d241d18b7a8f4b4c30095f5da9236a5e9e
anantapk03 <anantapk03@gmail.com>
2022-05-10 16:11:44 +0700

    telor 2.py

--- telor.py
+++ telor.py
@@ -3,7 +3,7 @@ from produk import Produk
 class Telur (Produk) :
     
     def show_produk (self) :
-        print ("Nama produk ", self.__nama , "Harga produk ", self.get_harga())
+        print ("Nama produk ", self.get_nama(), "Harga produk ", self.get_harga())
         
         
     
