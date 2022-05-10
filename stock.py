e55127b14eedfd507b57fc459258e8891b9962c7
harief285 <harief285@gmail.com>
2022-05-10 15:40:39 +0700

    Perubahan Arief Tambah Class Stock v2

--- stock.py
+++ stock.py
@@ -1,9 +1,10 @@
 class Stock:
-    def __init__(self, produk : None, qty: int):
+    def __init__(self, produk : Produk, qty: int):
         self.__produk=produk
         self.__qty=qty
         
-    def get_produk(self) -> None:
+    def get_produk(self) -> Produk:
         return self.__produk
     def get_qty(self) -> int:
         return self.__qty
+    
